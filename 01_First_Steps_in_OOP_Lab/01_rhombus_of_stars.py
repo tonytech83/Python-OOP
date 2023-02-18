aa1fc8263712177322e922137e6473bfba193901
Anton Petrov <a.petrov.83@gmail.com>
2023-02-18 19:14:56 +0200

    Updated solution

--- 01_First_Steps_in_OOP_Lab/01_rhombus_of_stars.py
+++ 01_First_Steps_in_OOP_Lab/01_rhombus_of_stars.py
@@ -9,22 +9,34 @@ class DrawRhombus:
         self.result = ''
 
     def main(self):
+        """
+        Main method calling methods for upper and lower parts.
+        :return: The finished rhombus.
+        """
         self.upper_part()
         self.lower_part()
         return self.result
 
     def upper_part(self):
+        """
+        This method call draw_rhombus method and prepare the upper part of rhombus.
+        """
         for row in range(1, self.size + 1):
             self.draw_rhombus(row)
 
     def lower_part(self):
+        """
+        This method call draw_rhombus method and prepare the lower part of rhombus.
+        :return:
+        """
         for row in range(self.size - 1, 0, -1):
             self.draw_rhombus(row)
 
-    def draw_rhombus(self, row):
-        self.result += ' ' * (self.size - row)
-        self.result += '* ' * row
-        self.result += '\n'
+    def draw_rhombus(self, row: int):
+        """
+        This func draws each row from rhombus.
+        """
+        self.result += (' ' * (self.size - row)) + ('* ' * row) + '\n'
 
 
 if __name__ == '__main__':
