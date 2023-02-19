8a905268ca647055554dfd33a4e6a7a902572335
Anton Petrov <a.petrov.83@gmail.com>
2023-02-19 17:58:32 +0200

    Added solution

---
+++ 02_First_Steps_in_OOP_Exercise/03_employee.py
@@ -0,0 +1,27 @@
+# Exam: 03. Employee
+# From: First Steps in OOP - Exercise
+# URL: https://judge.softuni.org/Contests/Practice/Index/1935#2
+
+class Employee:
+    def __init__(self, id: int, first_name: str, last_name: str, salary: int):
+        self.id = id
+        self.salary = salary
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_annual_salary(self):
+        return self.salary * 12
+
+    def raise_salary(self, amount):
+        self.salary += amount
+        return self.salary
+
+
+# Test code
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
