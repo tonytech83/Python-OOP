159f907fa5020909f0f08f4f3111d23c1406e0b8
Anton Petrov <a.petrov.83@gmail.com>
2023-02-19 17:33:26 +0200

    Added solution

---
+++ 02_First_Steps_in_OOP_Exercise/02_hero.py
@@ -0,0 +1,26 @@
+# Exam: 02. Hero
+# From: First Steps in OOP - Exercise
+# URL: https://judge.softuni.org/Contests/Practice/Index/1935#1
+
+class Hero:
+    def __init__(self, name: str, health: int):
+        self.name = name
+        self.health = health
+
+    def defend(self, damage):
+        self.health -= damage
+
+        if self.health <= 0:
+            self.health = 0
+            return f'{self.name} was defeated'
+
+    def heal(self, amount):
+        self.health += amount
+
+
+# Test code
+hero = Hero("Peter", 100)
+print(hero.defend(50))
+hero.heal(50)
+print(hero.defend(99))
+print(hero.defend(1))
