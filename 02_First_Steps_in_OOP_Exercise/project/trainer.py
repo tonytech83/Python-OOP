0ca6da626a9e685572f9aa1ac3641f04549d1e18
Anton Petrov <a.petrov.83@gmail.com>
2023-02-20 21:09:07 +0200

    Added solution for exam "08. Pokemon Battle"

---
+++ 02_First_Steps_in_OOP_Exercise/project/trainer.py
@@ -0,0 +1,29 @@
+from project.pokemon import Pokemon
+
+
+class Trainer:
+    def __init__(self, name):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if pokemon not in self.pokemons:
+            self.pokemons.append(pokemon)
+            return f'Caught {pokemon.pokemon_details()}'
+
+        return f'This pokemon is already caught'
+
+    def release_pokemon(self, pokemon_name: str):
+        for pokemon in self.pokemons:
+            if pokemon.name == pokemon_name:
+                self.pokemons.remove(pokemon)
+                return f'You have released {pokemon_name}'
+
+        return 'Pokemon is not caught'
+
+    def trainer_data(self):
+        result = f'Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}'
+        for pokemon in self.pokemons:
+            result += '\n' + f'- {pokemon.pokemon_details()}\n'
+
+        return result
