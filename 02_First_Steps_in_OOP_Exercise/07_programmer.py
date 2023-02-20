94686470d5a548bac313b0e193c3e30fc2c253b2
Anton Petrov <a.petrov.83@gmail.com>
2023-02-20 12:47:06 +0200

    Added solution

---
+++ 02_First_Steps_in_OOP_Exercise/07_programmer.py
@@ -0,0 +1,37 @@
+# Exam: 07. Programmer
+# From: First Steps in OOP - Exercise
+# URL: https://judge.softuni.org/Contests/Practice/Index/1935#6
+
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if language == self.language:
+            self.skills += skills_earned
+            return f'{self.name} watched {course_name}'
+
+        return f'{self.name} does not know {language}'
+
+    def change_language(self, new_language, skills_needed):
+        if self.language == new_language:
+            return f'{self.name} already knows {self.language}'
+
+        if skills_needed > self.skills:
+            return f'{self.name} needs {skills_needed - self.skills} more skills'
+
+        previous_language = self.language
+        self.language = new_language
+        return f'{self.name} switched from {previous_language} to {new_language}'
+
+
+# Test code
+programmer = Programmer("John", "Java", 50)
+print(programmer.watch_course("Python Masterclass", "Python", 84))
+print(programmer.change_language("Java", 30))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Java: zero to hero", "Java", 50))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Python Masterclass", "Python", 84))
