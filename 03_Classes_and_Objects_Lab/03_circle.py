874503a2890b975bee7db5507f83d2c38b198ddf
Anton Petrov <a.petrov.83@gmail.com>
2023-02-22 19:41:23 +0200

    Added solution

---
+++ 03_Classes_and_Objects_Lab/03_circle.py
@@ -0,0 +1,25 @@
+# Exam: 03. Circle
+# From: Classes and Objects - Lab
+# URL: https://judge.softuni.org/Contests/Practice/Index/1936#2
+
+class Circle:
+    pi = 3.14
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+    def get_area(self):
+        return Circle.pi * pow(self.radius, 2)
+
+    def get_circumference(self):
+        return 2 * Circle.pi * self.radius
+
+
+# Test code
+circle = Circle(10)
+circle.set_radius(12)
+print(circle.get_area())
+print(circle.get_circumference())
