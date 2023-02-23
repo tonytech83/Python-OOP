49f998b1eef49c4274b99d0e0bb2cb1127d4426f
Anton Petrov <a.petrov.83@gmail.com>
2023-02-23 21:04:28 +0200

    Updated solution

--- 02_First_Steps_in_OOP_Exercise/project/trainer.py
+++ 02_First_Steps_in_OOP_Exercise/project/trainer.py
@@ -1,28 +1,33 @@
-from project.pokemon import Pokemon
+from typing import List
+from pokemon import Pokemon
 
 
 class Trainer:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
-        self.pokemons = []
+        self.pokemons: List[Pokemon] = []
 
-    def add_pokemon(self, pokemon: Pokemon):
-        if any([x.name == pokemon.name for x in self.pokemons]):
+    def add_pokemon(self, pokemon: Pokemon) -> str:
+        if not any([x.name == pokemon.name for x in self.pokemons]):
             self.pokemons.append(pokemon)
+
             return f'Caught {pokemon.pokemon_details()}'
 
         return f'This pokemon is already caught'
 
-    def release_pokemon(self, pokemon_name: str):
-        for pokemon in self.pokemons:
-            if pokemon.name == pokemon_name:
-                self.pokemons.remove(pokemon)
-                return f'You have released {pokemon_name}'
+    def release_pokemon(self, pokemon_name: str) -> str:
+        try:
+            pokemon = next(filter(lambda p: p.name == pokemon_name, self.pokemons))
+        except StopIteration:
+            return 'Pokemon is not caught'
 
-        return 'Pokemon is not caught'
+        self.pokemons.remove(pokemon)
 
-    def trainer_data(self):
+        return f'You have released {pokemon_name}'
+
+    def trainer_data(self) -> str:
         result = f'Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}'
+
         for pokemon in self.pokemons:
             result += '\n' + f'- {pokemon.pokemon_details()}\n'
 
