4a63abb67e66b40ac06f73d4383224bb4d8ceac8
Anton Petrov <a.petrov.83@gmail.com>
2023-02-23 19:11:10 +0200

    Updated solution

--- 02_First_Steps_in_OOP_Exercise/03_employee.py
+++ 02_First_Steps_in_OOP_Exercise/03_employee.py
@@ -3,20 +3,23 @@
 # URL: https://judge.softuni.org/Contests/Practice/Index/1935#2
 
 class Employee:
-    def __init__(self, id: int, first_name: str, last_name: str, salary: int):
-        self.id = id
+    MONTHS = 12
+
+    def __init__(self, employee_id: int, first_name: str, last_name: str, salary: int):
+        self.id = employee_id
         self.salary = salary
         self.first_name = first_name
         self.last_name = last_name
 
-    def get_full_name(self):
+    def get_full_name(self) -> str:
         return f'{self.first_name} {self.last_name}'
 
-    def get_annual_salary(self):
-        return self.salary * 12
+    def get_annual_salary(self) -> int:
+        return self.salary * Employee.MONTHS
 
-    def raise_salary(self, amount):
+    def raise_salary(self, amount: int) -> int:
         self.salary += amount
+
         return self.salary
 
 
