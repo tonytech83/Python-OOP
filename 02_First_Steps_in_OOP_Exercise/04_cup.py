6a25f7d52b6a0b0d8a0eb81e48a0b0969836d93a
Anton Petrov <a.petrov.83@gmail.com>
2023-02-23 19:17:10 +0200

    Updated solution

--- 02_First_Steps_in_OOP_Exercise/04_cup.py
+++ 02_First_Steps_in_OOP_Exercise/04_cup.py
@@ -3,14 +3,14 @@
 # URL: https://judge.softuni.org/Contests/Practice/Index/1935#3
 
 class Cup:
-    def __init__(self, size, quantity):
+    def __init__(self, size: int, quantity: int):
         self.size = size
         self.quantity = quantity
 
-    def fill(self, amount):
+    def fill(self, amount: int) -> None:
         self.quantity += amount if self.status() >= amount else 0
 
-    def status(self):
+    def status(self) -> int:
         return self.size - self.quantity
 
 
