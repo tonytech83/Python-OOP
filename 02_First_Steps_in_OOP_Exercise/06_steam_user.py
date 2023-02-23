9068f635bde740eb48548049b9dbbf58c332ea43
Anton Petrov <a.petrov.83@gmail.com>
2023-02-23 19:39:56 +0200

    Updated solution

--- 02_First_Steps_in_OOP_Exercise/06_steam_user.py
+++ 02_First_Steps_in_OOP_Exercise/06_steam_user.py
@@ -2,27 +2,32 @@
 # From: First Steps in OOP - Exercise
 # URL: https://judge.softuni.org/Contests/Practice/Index/1935#5
 
+from typing import List
+
+
 class SteamUser:
-    def __init__(self, username, games):
+    def __init__(self, username: str, games: List[str]):
         self.username = username
         self.games = games
         self.played_hours = 0
 
-    def play(self, game, hours):
+    def play(self, game: str, hours: int) -> str:
         if game in self.games:
             self.played_hours += hours
+
             return f'{self.username} is playing {game}'
 
         return f'{game} is not in library'
 
-    def buy_game(self, game):
+    def buy_game(self, game: str) -> str:
         if game not in self.games:
             self.games.append(game)
+
             return f'{self.username} bought {game}'
 
         return f'{game} is already in your library'
 
-    def status(self):
+    def status(self) -> str:
         return f'{self.username} has {len(self.games)} games. Total play time: {self.played_hours}'
 
 
