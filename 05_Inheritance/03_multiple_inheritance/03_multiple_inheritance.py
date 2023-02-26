b69d709af1fa121a504371952bbb375422583768
Anton Petrov <a.petrov.83@gmail.com>
2023-02-26 15:58:16 +0200

    Added py file with some tests

---
+++ 05_Inheritance/03_multiple_inheritance/03_multiple_inheritance.py
@@ -0,0 +1,11 @@
+# Exam: 03. Multiple Inheritance
+# From: Inheritance - Lab
+# URL: https://judge.softuni.org/Contests/Practice/Index/1940#2
+
+from project.teacher import Teacher
+
+# Test code
+t = Teacher()
+print(t.teach())
+print(t.sleep())
+print(t.get_fired())
