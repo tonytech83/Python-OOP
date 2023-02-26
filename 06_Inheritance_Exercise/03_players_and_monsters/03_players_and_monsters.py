0f18d94721951408fe9a978c7c34eee1cc5ee8f8
Anton Petrov <a.petrov.83@gmail.com>
2023-02-26 17:28:44 +0200

    Added solution

---
+++ 06_Inheritance_Exercise/03_players_and_monsters/03_players_and_monsters.py
@@ -0,0 +1,17 @@
+# Exam: 02. Zoo
+# From: Inheritance - Exercise
+# URL: https://judge.softuni.org/Contests/Compete/Index/1941#0
+from project.elf import Elf
+from project.hero import Hero
+
+# Test code
+hero = Hero("H", 4)
+print(hero.username)
+print(hero.level)
+print(str(hero))
+elf = Elf("E", 4)
+print(str(elf))
+print(elf.__class__.__bases__[0].__name__)
+print(elf.username)
+print(elf.level)
+
