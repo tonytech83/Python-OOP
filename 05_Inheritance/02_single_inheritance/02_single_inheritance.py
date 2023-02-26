1d667ceef891e51cee4de9aaf004251a704ae7a5
Anton Petrov <a.petrov.83@gmail.com>
2023-02-26 15:59:38 +0200

    Added py file with some tests

---
+++ 05_Inheritance/02_single_inheritance/02_single_inheritance.py
@@ -0,0 +1,10 @@
+# Exam: 02. Single Inheritance
+# From: Inheritance - Lab
+# URL: https://judge.softuni.org/Contests/Practice/Index/1940#1
+
+from project.dog import Dog
+
+# ----- Test code -----
+dog = Dog()
+print(dog.eat())
+print(dog.bark())
--- 05_Inheritance/02_single_inheritance/project/dog.py
+++ 05_Inheritance/02_single_inheritance/project/dog.py
@@ -5,9 +5,3 @@ class Dog(Animal):
     @staticmethod
     def bark():
         return 'barking...'
-
-
-# ----- Test code -----
-# dog = Dog()
-# print(dog.eat())
-# print(dog.bark())
