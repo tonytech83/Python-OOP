5da27b17833fef2c3e3bb6b622b61f93e3cb03c2
Anton Petrov <a.petrov.83@gmail.com>
2023-02-26 16:00:45 +0200

    Added py file with some tests

---
+++ 05_Inheritance/01_food/01_food.py
@@ -0,0 +1,11 @@
+# Exam: 01. Food
+# From: Inheritance - Lab
+# URL: https://judge.softuni.org/Contests/Practice/Index/1940#0
+
+from project.fruit import Fruit
+
+# ------- Test code -------
+fruit = Fruit('apple', '10-12-2023')
+
+print(fruit.name)
+print(fruit.expiration_date)
--- 05_Inheritance/01_food/project/fruit.py
+++ 05_Inheritance/01_food/project/fruit.py
@@ -5,9 +5,3 @@ class Fruit(Food):
     def __init__(self, name: str, expiration_date: str):
         super().__init__(expiration_date)
         self.name = name
-
-# ------- Test code -------
-# fruit = Fruit('apple', '10-12-2023')
-#
-# print(fruit.name)
-# print(fruit.expiration_date)
