7450f8ec262a5007a679746b1679aeb42f263710
Anton Petrov <a.petrov.83@gmail.com>
2023-02-26 14:51:36 +0200

    Added solution

---
+++ 05_Inheritance/05_hierarchical_inheritance/05_hierarchical_inheritance.py
@@ -0,0 +1,15 @@
+# Exam: 05. Hierarchical Inheritance
+# From: Inheritance - Lab
+# URL: https://judge.softuni.org/Contests/Practice/Index/1940#4
+
+from project.cat import Cat
+from project.dog import Dog
+
+# Test code
+cat = Cat()
+print(cat.eat())
+print(cat.meow())
+
+dog = Dog()
+print(dog.eat())
+print(dog.bark())
