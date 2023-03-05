d025e84b5ce066b48f419d9ea6fbce6abe4cf11b
Anton Petrov <a.petrov.83@gmail.com>
2023-03-05 15:40:19 +0200

    Updated attributes to be private.

--- 11_Polymorphism_and_Abstraction_Lab/04_shapes.py
+++ 11_Polymorphism_and_Abstraction_Lab/04_shapes.py
@@ -18,25 +18,25 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius: int):
-        self._radius = radius
+        self.__radius = radius
 
     def calculate_area(self):
-        return math.pi * self._radius ** 2
+        return math.pi * self.__radius ** 2
 
     def calculate_perimeter(self):
-        return 2 * math.pi * self._radius
+        return 2 * math.pi * self.__radius
 
 
 class Rectangle(Shape):
     def __init__(self, height: int, width: int):
-        self._height = height
-        self._width = width
+        self.__height = height
+        self.__width = width
 
     def calculate_area(self):
-        return self._width * self._height
+        return self.__width * self.__height
 
     def calculate_perimeter(self):
-        return 2 * (self._width + self._height)
+        return 2 * (self.__width + self.__height)
 
 
 # Test code 1
