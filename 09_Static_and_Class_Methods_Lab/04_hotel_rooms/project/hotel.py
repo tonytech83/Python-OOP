a2d5175281eb5a23fa4ef2f5892be3b5faeeeed1
Anton Petrov <a.petrov.83@gmail.com>
2023-03-08 09:29:58 +0200

    Update class Hotel

--- 09_Static_and_Class_Methods_Lab/04_hotel_rooms/project/hotel.py
+++ 09_Static_and_Class_Methods_Lab/04_hotel_rooms/project/hotel.py
@@ -1,32 +1,33 @@
+from typing import List
 from project.room import Room
 
 
 class Hotel:
     def __init__(self, name: str):
         self.name = name
-        self.rooms: list = []
+        self.rooms: List[Room] = []
 
     @property
     def guests(self) -> int:
         return sum([room.guests for room in self.rooms])
 
     @classmethod
-    def from_stars(cls, stars_count: int) -> Room:
+    def from_stars(cls, stars_count: int):
         return cls(f'{stars_count} stars Hotel')
 
     def add_room(self, room: Room):
         self.rooms.append(room)
 
-    def take_room(self, room_number: int, people: int):
+    def take_room(self, room_number: int, people: int) -> str or None:
         return self.__found_room(room_number).take_room(people)
 
-    def free_room(self, room_number: int):
+    def free_room(self, room_number: int) -> str or None:
         return self.__found_room(room_number).free_room()
 
     def __found_room(self, room_number: int) -> Room:
         return [room for room in self.rooms if room.number == room_number][0]
 
-    def status(self):
+    def status(self) -> str:
         result = f'Hotel {self.name} has {self.guests} total guests\n' \
                  f'Free rooms: {", ".join([str(room.number) for room in self.rooms if not room.is_taken])}\n' \
                  f'Taken rooms: {", ".join([str(room.number) for room in self.rooms if room.is_taken])}'
