81eacda9bb9f6f2b47afb8e71ad00f8044ef51a2
Anton Petrov <a.petrov.83@gmail.com>
2023-03-10 20:08:57 +0200

    Added solution

---
+++ 12_Polymorphism_and_Abstraction_Exercise/05_animals/project/cat.py
@@ -0,0 +1,10 @@
+from project.animal import Animal
+
+
+class Cat(Animal):
+    def __init__(self, name: str, age: int, gender: str):
+        super().__init__(name, age, gender)
+
+    def make_sound(self):
+        return 'Meow meow!'
+
---
+++ 12_Polymorphism_and_Abstraction_Exercise/05_animals/project/kitten.py
@@ -0,0 +1,9 @@
+from project.cat import Cat
+
+
+class Kitten(Cat):
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age, 'Female')
+
+    def make_sound(self):
+        return 'Meow'
