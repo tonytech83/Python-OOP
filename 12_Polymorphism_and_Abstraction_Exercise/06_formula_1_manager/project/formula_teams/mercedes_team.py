0da29967c040a7a80ad09415a3241e823b034387
Anton Petrov <a.petrov.83@gmail.com>
2023-03-10 21:59:25 +0200

    remove commented code

--- 12_Polymorphism_and_Abstraction_Exercise/06_formula_1_manager/project/formula_teams/mercedes_team.py
+++ 12_Polymorphism_and_Abstraction_Exercise/06_formula_1_manager/project/formula_teams/mercedes_team.py
@@ -13,22 +13,3 @@ class MercedesTeam(FormulaTeam):
             7: 50000
         }
     }
-
-    # def calculate_revenue_after_race(self, race_pos: int):
-    #     # first_sponsors = {
-    #     #     1: 1000000,
-    #     #     3: 500000,
-    #     # }
-    #     # second_sponsor = {
-    #     #     5: 100000,
-    #     #     7: 50000
-    #     # }
-    #     #
-    #     # first_sponsors_reward = max([first_sponsors[place] if race_pos <= place else 0 for place in first_sponsors])
-    #     # second_sponsor_reward = max([second_sponsor[place] if race_pos <= place else 0 for place in second_sponsor])
-    #     #
-    #     # revenue = (first_sponsors_reward + second_sponsor_reward) - MercedesTeam.EXPENSES_PER_RACE
-    #     # self.budget += revenue
-    #     #
-    #     # return f'The revenue after the race is {revenue}$. Current budget {self.budget}$'
-    #     pass
--- 12_Polymorphism_and_Abstraction_Exercise/06_formula_1_manager/project/formula_teams/red_bull_team.py
+++ 12_Polymorphism_and_Abstraction_Exercise/06_formula_1_manager/project/formula_teams/red_bull_team.py
@@ -13,22 +13,3 @@ class RedBullTeam(FormulaTeam):
             10: 10000
         }
     }
-
-    # def calculate_revenue_after_race(self, race_pos: int):
-        # first_sponsors = {
-        #     1: 1500000,
-        #     2: 800000,
-        # }
-        # second_sponsor = {
-        #     8: 20000,
-        #     10: 10000
-        # }
-        #
-        # first_sponsors_reward = max([first_sponsors[place] if race_pos <= place else 0 for place in first_sponsors])
-        # second_sponsor_reward = max([second_sponsor[place] if race_pos <= place else 0 for place in second_sponsor])
-        #
-        # revenue = (first_sponsors_reward + second_sponsor_reward) - RedBullTeam.EXPENSES_PER_RACE
-        # self.budget += revenue
-        #
-        # return f'The revenue after the race is {revenue}$. Current budget {self.budget}$'
-        # pass
