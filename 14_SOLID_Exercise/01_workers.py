7e7294043f86bd82192fff5baa2005fd3ae08e3f
Anton Petrov <a.petrov.83@gmail.com>
2023-03-16 19:38:29 +0200

    Refactored exam using Dependency Inversion Principle

---
+++ 14_SOLID_Exercise/01_workers.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+
+class BaseWorker(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def work():
+        pass
+
+
+class Worker(BaseWorker):
+
+    @staticmethod
+    def work():
+        print("I'm working!!")
+
+
+class SuperWorker(BaseWorker):
+
+    @staticmethod
+    def work():
+        print("I work very hard!!!")
+
+
+class Manager:
+
+    def __init__(self):
+        self.worker = None
+
+    def set_worker(self, worker):
+        assert isinstance(worker, BaseWorker), f'`worker` must be of type {worker}'
+
+        self.worker = worker
+
+    def manage(self):
+        if self.worker is not None:
+            self.worker.work()
+
+
+worker = Worker()
+manager = Manager()
+manager.set_worker(worker)
+manager.manage()
+
+super_worker = SuperWorker()
+try:
+    manager.set_worker(super_worker)
+    manager.manage()
+except AssertionError:
+    print("manager fails to support super_worker....")
