7dfd5abf5586457771aa8089a75a1ffa92445f99
Anton Petrov <a.petrov.83@gmail.com>
2023-03-20 20:14:25 +0200

    Added solution

---
+++ 17_Decorators_Lab/03_even_numbers.py
@@ -0,0 +1,17 @@
+# Exam: 03. Even Numbers
+# From: Decorators - Lab
+# URL: https://judge.softuni.org/Contests/Practice/Index/1946#2
+
+def even_numbers(function):
+    def wrapper(numbers):
+        return [num for num in numbers if num % 2 == 0]
+
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
