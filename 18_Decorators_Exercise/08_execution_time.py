42a5a3f229bd52752b201ab74739c2dc778bf3b4
Anton Petrov <a.petrov.83@gmail.com>
2023-03-22 16:22:32 +0200

    Added solution

---
+++ 18_Decorators_Exercise/08_execution_time.py
@@ -0,0 +1,51 @@
+# Exam: 08.	Execution Time
+# From: Decorators - Exercise
+# URL: N/A
+
+from time import time
+
+
+def exec_time(func_ref):
+    def wrapper(*args):
+        start = time()
+        func_ref(*args)
+        end = time()
+
+        return end - start
+
+    return wrapper
+
+
+# Test code 1
+@exec_time
+def loop(start, end):
+    total = 0
+    for x in range(start, end):
+        total += x
+    return total
+
+
+print(loop(1, 10000000))
+
+
+# Test code 2
+@exec_time
+def concatenate(strings):
+    result = ""
+    for string in strings:
+        result += string
+    return result
+
+
+print(concatenate(["a" for i in range(1000000)]))
+
+
+# Test code 3
+@exec_time
+def loop():
+    count = 0
+    for i in range(1, 9999999):
+        count += 1
+
+
+print(loop())
